8289a5dac9869a389f7c910d19e32f5d16b751ab
Vasili <terkin.vasja@gmail.com>
2017-01-20 22:39:19 +0300

    Initial commit

---
+++ src/main/java/by/kutsko/ShoppinglistApplication.java
@@ -0,0 +1,12 @@
+package by.kutsko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppinglistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppinglistApplication.class, args);
+	}
+}
