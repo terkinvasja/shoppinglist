18dbe62f8befc3fffd3544a121dd686dd7f3bc74
Vasili <terkin.vasja@gmail.com>
2017-01-23 22:53:57 +0300

    Complied Abstract User Service Test

--- src/test/java/by/kutsko/service/AbstractUserServiceTest.java
+++ src/test/java/by/kutsko/service/AbstractUserServiceTest.java
@@ -2,6 +2,7 @@ package by.kutsko.service;
 
 import by.kutsko.ShoppinglistApplication;
 import by.kutsko.domain.User;
+import by.kutsko.util.exception.NotFoundException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -15,6 +16,7 @@ import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 
 import static by.kutsko.UserTestData.*;
@@ -48,5 +50,41 @@ public class AbstractUserServiceTest {
     public void testDelete() throws Exception {
         service.delete(USER_ID);
         MATCHER.assertCollectionEquals(Collections.singletonList(ADMIN), service.getAll());
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void testNotFoundDelete() throws Exception {
+        service.delete(1);
+    }
+
+    @Test
+    public void testGet() throws Exception {
+        User user = service.get(ADMIN_ID);
+        MATCHER.assertEquals(ADMIN, user);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void testGetNotFound() throws Exception {
+        service.get(1);
+    }
+
+    @Test
+    public void testGetByEmail() throws Exception {
+        User user = service.getByEmail("admin@gmail.com");
+        MATCHER.assertEquals(ADMIN, user);
+    }
+
+    @Test
+    public void testGetAll() throws Exception {
+        Collection<User> all = service.getAll();
+        MATCHER.assertCollectionEquals(Arrays.asList(ADMIN, USER), all);
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        User updated = new User(USER);
+        updated.setName("UpdatedName");
+        service.update(updated);
+        MATCHER.assertEquals(updated, service.get(USER_ID));
     }
 }
